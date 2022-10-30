150b533d9927a8a50f2d766e2198feb581ecdead
Ashley <99642112+Ashley1321@users.noreply.github.com>
2022-10-30 21:45:21 +0200

    Add files via upload

--- App.js
+++ App.js
@@ -2,14 +2,15 @@ import * as React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-import RcHome from "./components/receptionist/rcHome";
+// import RcHome from "./components/receptionist/rcHome";
 import LoginAs from "./components/loginAs";
 import Register from "./components/receptionist/register";
-import RcPatientFile from "./components/receptionist/rcPatientFile";
+// import RcPatientFile from "./components/receptionist/rcPatientFile";
 import NurseHome from "./components/nurse/nurseHome";
-import RcLogin from "./components/receptionist/rcLogin";
+// import RcLogin from "./components/receptionist/rcLogin";
 import NursePatientFile from "./components/nurse/nursePatientFile";
-
+import NurseLogin from "./components/nurse/nurseLogIn";
+import DoctorLogin from "./components/doctor/doctorLogin";
 const App = () => {
   const Stack = createNativeStackNavigator();
   return (
@@ -20,12 +21,14 @@ const App = () => {
         }}
       >
         <Stack.Screen name="loginAs" component={LoginAs} />
-        <Stack.Screen name="rcLogin" component={RcLogin} />
+        {/* <Stack.Screen name="rcLogin" component={RcLogin} /> */}
         <Stack.Screen name="register" component={Register} />
-        <Stack.Screen name="rcHome" component={RcHome} />
-        <Stack.Screen name="rcPatientFile" component={RcPatientFile} />
+        {/* <Stack.Screen name="rcHome" component={RcHome} /> */}
+        {/* <Stack.Screen name="rcPatientFile" component={RcPatientFile} /> */}
         <Stack.Screen name="nursePatientFile" component={NursePatientFile} />
         <Stack.Screen name="nurseHome" component={NurseHome} />
+        <Stack.Screen name="nurseLogIn" component={NurseLogin} />
+        <Stack.Screen name="doctorLogin" component={DoctorLogin} />
       </Stack.Navigator>
     </NavigationContainer>
   );
